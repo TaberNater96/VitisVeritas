531df40daf2509dea095b071ea8a5108582764a8
Elijah Taber <boxcar2taber@yahoo.com>
2025-08-05 16:04:07 -0700

    Update gitignore and map canvas icon sizes. Ingest first set of soil data.

--- frontend/src/components/map/MapCanvas.jsx
+++ frontend/src/components/map/MapCanvas.jsx
@@ -176,13 +176,13 @@ const MapCanvas = () => {
           source: 'wineries',
           layout: {
             'icon-image': 'winery-icon',
-            'icon-size': 0.045,
+            'icon-size': 0.035,
             'icon-allow-overlap': true,
             'icon-ignore-placement': false,
             'icon-anchor': 'bottom' // anchor to bottom so the point is at the actual location
           },
           paint: {
-            'icon-opacity': 1.0 // Ensure full opacity
+            'icon-opacity': 1.0 // full opacity
           }
         });
 
